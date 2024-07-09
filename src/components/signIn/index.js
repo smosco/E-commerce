2370dd895b5e27e9adb0218280bb40901c243c2b
smosco <go1ruf2tk3@gmail.com>
2024-07-09 16:12:24 +0900

    feat: 이메일, 비밀번호 로그인 추가

--- src/components/signIn/index.js
+++ src/components/signIn/index.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './styles.scss';
 import Button from '../forms/button';
-import { signInWithGoogle } from '../../firebase/utils';
+import { signInWithGoogle, auth } from '../../firebase/utils';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+
+import FormInput from '../forms/formInput';
 
 const SignIn = () => {
+  const initialState = {
+    email: '',
+    password: '',
+  };
+
+  const [formData, setFormData] = useState(initialState);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    const { email, password } = formData;
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      setFormData({
+        ...initialState,
+      });
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   return (
@@ -15,6 +43,22 @@ const SignIn = () => {
 
         <div className='formWrap'>
           <form onSubmit={handleSubmit}>
+            <FormInput
+              type='email'
+              name='email'
+              value={formData.email}
+              placeholder='Email'
+              handleChange={handleChange}
+            />
+            <FormInput
+              type='password'
+              name='password'
+              value={formData.password}
+              placeholder='Password'
+              handleChange={handleChange}
+            />
+            <Button type='submit'>LogIn</Button>
+
             <div className='socialSignin'>
               <div className='row'>
                 <Button onClick={signInWithGoogle}>Sign in with Google</Button>
