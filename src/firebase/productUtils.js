a08dc7c8b5775789c3267ccf60037de8e695dc58
smosco <go1ruf2tk3@gmail.com>
2024-07-11 17:33:42 +0900

    feat: firebase와 연동하여 상품 추가, 삭제, 조회 기능 구현

---
+++ src/firebase/productUtils.js
@@ -0,0 +1,61 @@
+import { firestore } from './utils';
+import {
+  collection,
+  doc,
+  setDoc,
+  getDoc,
+  getDocs,
+  deleteDoc,
+} from 'firebase/firestore';
+
+export const handleAddProduct = async (product) => {
+  try {
+    const docRef = doc(collection(firestore, 'products'));
+    await setDoc(docRef, product);
+  } catch (err) {
+    console.error('Error adding product: ', err);
+  }
+};
+
+export const handleFetchProducts = async () => {
+  try {
+    const docRef = collection(firestore, 'products');
+    const snapshot = await getDocs(docRef);
+
+    const data = snapshot.docs.map((doc) => ({
+      ...doc.data(),
+      documentID: doc.id,
+    }));
+
+    return {
+      data,
+    };
+  } catch (err) {
+    console.error('Error fetching products: ', err);
+  }
+};
+
+export const handleDeleteProduct = async (documentID) => {
+  try {
+    const docRef = doc(firestore, 'products', documentID);
+    await deleteDoc(docRef);
+  } catch (err) {
+    console.error('Error deleting product: ', err);
+  }
+};
+
+export const handleFetchProduct = async (productID) => {
+  try {
+    const docRef = doc(firestore, 'products', productID);
+    const snapshot = await getDoc(docRef);
+
+    if (snapshot.exists()) {
+      return {
+        ...snapshot.data(),
+        documentID: productID,
+      };
+    }
+  } catch (err) {
+    console.error('Error fetching product: ', err);
+  }
+};
