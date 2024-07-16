d0303d26f0a5aecc11633e4b089b54430a5a106c
smosco <go1ruf2tk3@gmail.com>
2024-07-16 12:51:17 +0900

    feat: zustand로 cartItems 상태 관리 로직 구현

---
+++ src/zustand/cartStore.js
@@ -0,0 +1,60 @@
+import { create } from 'zustand';
+
+const useCartStore = create((set) => ({
+  cartItems: [],
+
+  addToCart: (nextCartItem) =>
+    set((state) => {
+      const cartItemExists = state.cartItems.find(
+        (cartItem) => cartItem.documentID === nextCartItem.documentID
+      );
+
+      if (cartItemExists) {
+        return {
+          cartItems: state.cartItems.map((cartItem) =>
+            cartItem.documentID === nextCartItem.documentID
+              ? { ...cartItem, quantity: cartItem.quantity + 1 }
+              : cartItem
+          ),
+        };
+      } else {
+        return {
+          cartItems: [...state.cartItems, { ...nextCartItem, quantity: 1 }],
+        };
+      }
+    }),
+
+  reduceCartItem: (cartItemToReduce) =>
+    set((state) => {
+      const existingCartItem = state.cartItems.find(
+        (cartItem) => cartItem.documentID === cartItemToReduce.documentID
+      );
+
+      if (existingCartItem.quantity === 1) {
+        return {
+          cartItems: state.cartItems.filter(
+            (cartItem) => cartItem.documentID !== existingCartItem.documentID
+          ),
+        };
+      } else {
+        return {
+          cartItems: state.cartItems.map((cartItem) =>
+            cartItem.documentID === existingCartItem.documentID
+              ? { ...cartItem, quantity: cartItem.quantity - 1 }
+              : cartItem
+          ),
+        };
+      }
+    }),
+
+  removeCartItem: (cartItemToRemove) =>
+    set((state) => ({
+      cartItems: state.cartItems.filter(
+        (item) => item.documentID !== cartItemToRemove.documentID
+      ),
+    })),
+
+  clearCart: () => set({ cartItems: [] }),
+}));
+
+export default useCartStore;
