1df9fdc8bddaac2f290de807ddd5290825c07b27
smosco <go1ruf2tk3@gmail.com>
2024-07-17 14:38:45 +0900

    feat: zustand middleware를 사용해 cart 상태를 로컬 스토리지에 저장함

--- src/zustand/cartStore.js
+++ src/zustand/cartStore.js
@@ -1,73 +1,83 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
-const useCartStore = create((set) => ({
-  cartItems: [],
+const useCartStore = create(
+  persist(
+    (set) => ({
+      cartItems: [],
 
-  addToCart: (nextCartItem) =>
-    set((state) => {
-      const cartItemExists = state.cartItems.find(
-        (cartItem) => cartItem.documentID === nextCartItem.documentID
-      );
+      addToCart: (nextCartItem) =>
+        set((state) => {
+          const cartItemExists = state.cartItems.find(
+            (cartItem) => cartItem.documentID === nextCartItem.documentID
+          );
 
-      if (cartItemExists) {
-        return {
-          cartItems: state.cartItems.map((cartItem) =>
-            cartItem.documentID === nextCartItem.documentID
-              ? { ...cartItem, quantity: cartItem.quantity + 1 }
-              : cartItem
-          ),
-        };
-      } else {
-        return {
-          cartItems: [...state.cartItems, { ...nextCartItem, quantity: 1 }],
-        };
-      }
-    }),
+          if (cartItemExists) {
+            return {
+              cartItems: state.cartItems.map((cartItem) =>
+                cartItem.documentID === nextCartItem.documentID
+                  ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                  : cartItem
+              ),
+            };
+          } else {
+            return {
+              cartItems: [...state.cartItems, { ...nextCartItem, quantity: 1 }],
+            };
+          }
+        }),
 
-  reduceCartItem: (cartItemToReduce) =>
-    set((state) => {
-      const existingCartItem = state.cartItems.find(
-        (cartItem) => cartItem.documentID === cartItemToReduce.documentID
-      );
+      reduceCartItem: (cartItemToReduce) =>
+        set((state) => {
+          const existingCartItem = state.cartItems.find(
+            (cartItem) => cartItem.documentID === cartItemToReduce.documentID
+          );
 
-      if (existingCartItem.quantity === 1) {
-        return {
+          if (existingCartItem.quantity === 1) {
+            return {
+              cartItems: state.cartItems.filter(
+                (cartItem) =>
+                  cartItem.documentID !== existingCartItem.documentID
+              ),
+            };
+          }
+
+          return {
+            cartItems: state.cartItems.map((cartItem) =>
+              cartItem.documentID === existingCartItem.documentID
+                ? { ...cartItem, quantity: cartItem.quantity - 1 }
+                : cartItem
+            ),
+          };
+        }),
+
+      removeCartItem: (cartItemToRemove) =>
+        set((state) => ({
           cartItems: state.cartItems.filter(
-            (cartItem) => cartItem.documentID !== existingCartItem.documentID
+            (item) => item.documentID !== cartItemToRemove.documentID
           ),
-        };
-      }
+        })),
 
-      return {
-        cartItems: state.cartItems.map((cartItem) =>
-          cartItem.documentID === existingCartItem.documentID
-            ? { ...cartItem, quantity: cartItem.quantity - 1 }
-            : cartItem
+      clearCart: () => set({ cartItems: [] }),
+
+      selectCartItemsCount: (state) =>
+        state.cartItems.reduce(
+          (accumulatedQuantity, cartItem) =>
+            accumulatedQuantity + cartItem.quantity,
+          0
         ),
-      };
+
+      selectCartTotalPrice: (state) =>
+        state.cartItems.reduce(
+          (total, cartItem) => total + cartItem.quantity * cartItem.price,
+          0
+        ),
     }),
-
-  removeCartItem: (cartItemToRemove) =>
-    set((state) => ({
-      cartItems: state.cartItems.filter(
-        (item) => item.documentID !== cartItemToRemove.documentID
-      ),
-    })),
-
-  clearCart: () => set({ cartItems: [] }),
-
-  selectCartItemsCount: (state) =>
-    state.cartItems.reduce(
-      (accumulatedQuantity, cartItem) =>
-        accumulatedQuantity + cartItem.quantity,
-      0
-    ),
-
-  selectCartTotalPrice: (state) =>
-    state.cartItems.reduce(
-      (total, cartItem) => total + cartItem.quantity * cartItem.price,
-      0
-    ),
-}));
+    {
+      name: 'cart-storage',
+      getStorage: () => localStorage,
+    }
+  )
+);
 
 export default useCartStore;
