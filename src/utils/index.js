366572f171ae241f68b7400c0f14b7f13a199729
smosco <go1ruf2tk3@gmail.com>
2024-07-18 11:18:50 +0900

    feat: axios 인스턴스 생성

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,6 +1,12 @@
+import axios from 'axios';
+
 export const checkUserIsAdmin = (currentUser) => {
   if (!currentUser || !Array.isArray(currentUser.userRoles)) return false;
 
   const { userRoles } = currentUser;
   return userRoles.includes('admin');
 };
+
+export const apiInstance = axios.create({
+  baseURL: process.env.REACT_APP_API_BASE_URL,
+});
