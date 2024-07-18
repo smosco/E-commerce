ef27da57dc63e5ab9d5b2e34e78a4c2295b77241
smosco <go1ruf2tk3@gmail.com>
2024-07-18 11:13:32 +0900

    feat: stripe 설정 추가

--- src/pages/payment/index.js
+++ src/pages/payment/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import PaymentDetails from '../../components/paymentDetails';
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
+import { publishableKey } from '../../stripe/config';
 
 const Payment = () => {
-  return <PaymentDetails />;
+  const stripePromise = loadStripe(publishableKey);
+
+  return (
+    <Elements stripe={stripePromise}>
+      <PaymentDetails />
+    </Elements>
+  );
 };
 
 export default Payment;
---
+++ src/stripe/config.js
@@ -0,0 +1,1 @@
+export const publishableKey = process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY;
