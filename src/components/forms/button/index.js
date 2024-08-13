3723565903589a247436a1618bf1fb1274ef193d
smosco <go1ruf2tk3@gmail.com>
2024-08-13 16:43:44 +0900

    design: 버튼 스타일 개선

--- src/components/forms/button/index.js
+++ src/components/forms/button/index.js
@@ -3,7 +3,7 @@ import './styles.scss';
 
 const Button = ({ children, ...otherProps }) => {
   return (
-    <button className='btn' {...otherProps}>
+    <button className='btn' aria-label='Button' {...otherProps}>
       {children}
     </button>
   );
